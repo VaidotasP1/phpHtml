2f87e7b0840f06b4c0f40f658dcfc3fe5bfaf190
Vaidotas Poskus <vaidotas963@inbox.lt>
2016-04-04 22:08:55 +0300

    Trace sql constants from settings.php to db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,5 +1,10 @@
 <?php
-require_once('settings.php');
+// duomenu bazes nustatymai
+define('SQL_HOST','localhost');
+define('SQL_DB','Books');
+define('SQL_USER','root');
+define('SQL_PASSWORD','root');
+
 $connection = mysqli_connect(SQL_HOST,SQL_USER,SQL_PASSWORD,SQL_DB);
 
 if (!$connection)
--- settings.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// duomenu bazes nustatymai
-define('SQL_HOST','localhost');
-define('SQL_DB','Books');
-define('SQL_USER','root');
-define('SQL_PASSWORD','root');
-?>
