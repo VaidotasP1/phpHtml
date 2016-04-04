6023a6a9db60dd50b86668414c5c3baf1789c185
Vaidotas Poskus <vaidotas963@inbox.lt>
2016-04-04 18:46:23 +0300

    Add Settings and connection to database

---
+++ db_connection.php
@@ -0,0 +1,11 @@
+<?php
+require_once('settings.php');
+$connection = mysqli_connect(SQL_HOST,SQL_USER,SQL_PASSWORD,SQL_DB);
+
+if (!$connection)
+{
+die('Neprisijungta : ' . mysqli_error());
+}
+
+
+?>
