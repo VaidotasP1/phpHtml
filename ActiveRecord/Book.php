56174c3d451943be3f27cdd70cbaee68ef3d314d
Vaidotas Poskus <vaidotas963@inbox.lt>
2016-04-05 22:26:11 +0300

    New function in Book class

--- ActiveRecord/Book.php
+++ ActiveRecord/Book.php
@@ -126,6 +126,31 @@ class Book
 
         }
     }
+    public function getAllBooks(){
+        $connection = mysqli_connect(self::SQL_HOST,self::SQL_USER,self::SQL_PASSWORD,self::SQL_DB);
+
+        if (!$connection)
+        {
+            die('Neprisijungta : ' . mysqli_connect_error());
+        }
+        mysqli_set_charset($connection,"utf8");
+        $sql="SELECT `Books`.`bookId`,`Books`.`title`,`BooksGenres`.`genre`,`Books`.`year`, GROUP_CONCAT(`Authors`.`name`) AS `bookAuthors` "
+            ." FROM `Books` "
+            ."JOIN `BooksMap` ON `BooksMap`.`bookId`=`Books`.`bookId` "
+            ."JOIN `Authors` ON `Authors`.`authorId`=`BooksMap`.`authorId` "
+            ."LEFT JOIN `BooksGenres` ON `BooksGenres`.`genreId`=`Books`.`genreId` "
+            ."GROUP BY `BooksMap`.`bookId`";
+        if($r=mysqli_query($connection,$sql)){
+            while ($books=mysqli_fetch_object($r)) {
+
+                $this->setAuthor($books->bookAuthors);
+                $this->setTitle($books->title);
+                $this->setYear($books->year);
+                $this->setBookId($books->bookId);
+                $this->setGenre($books->genre);
+            }
+        }
+    }
 
 }
 ?>
--- ActiveRecord/BooksList.php
+++ ActiveRecord/BooksList.php
@@ -1,12 +1,42 @@
 <?php
+include 'Book.php';
+$i=1;
+$books = new Book();
+$bookList=$books->getAllBooks();
 
-/**
- * Created by PhpStorm.
- * User: vaidotas
- * Date: 16.4.5
- * Time: 18.18
- */
-class BooksList
-{
-
-}
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/html">
+<head>
+    <title>Books list</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+<body>
+<table border="1" style="width:100%">
+    <tr>
+        <th>Num.</th>
+        <th>Name</th>
+        <th>Authors</th>
+        <th>Genre</th>
+        <th>Year</th>
+    </tr>
+    <?php
+        foreach ($bookList as $book)
+        {
+            /* @var Book $book*/
+            ?>
+            <tr>
+                <td><?php echo $i; ?> </td>
+                <td><a href="bookDetail.php?bookId=<?php echo $book->getBookId(); ?>"><?php echo $book->getTitle(); ?></a>  </td>
+                <td><?php echo $book->getAuthor(); ?> </td>
+                <td><?php echo $book->getGenre();  ?> </td>
+                <td><?php echo $book->getYear();  ?> </td>
+            </tr>
+            <?php
+            $i++;
+        }
+    ?>
+</table>
+</body>
+</html>
